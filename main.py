9f4a344ff52f050e5603f0e97bd62ea97f7f2dc1
catwithasword <panida.r.work@gmail.com>
2026-02-18 14:26:24 +0700

    feat: todo REST services

---
+++ main.py
@@ -0,0 +1,15 @@
+"""FastAPI implementation of the Todo REST API."""
+
+from fastapi import FastAPI
+
+from routers import todos
+
+
+# 'app' is refers to FastAPI
+# use param: redirect_slashes=False to disable automatic
+# redirection of paths without trailing slash.
+app = FastAPI(title="Todo REST API")
+
+
+### REST service URLs and request handlers ###
+app.include_router(todos.router)
